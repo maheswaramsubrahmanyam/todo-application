a91d2f68694b52d46af13d1bfc7ecd20d494427b
MAHESWARAM M V SUBRAHMANYAM <153249049+maheswaramsubrahmanyam@users.noreply.github.com>
2024-09-22 10:33:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// let checkBoxEl = document.createElement("input");
+// checkBoxEl.type = "checkbox";
+// checkBoxEl.id = "checkboxId";
+// document.body.appendChild(checkBoxEl);
+
+// let labelEl = document.createElement("label");
+// labelEl.setAttribute("for","checkboxId");
+// labelEl.textContent = "check box";
+// document.body.appendChild(labelEl);
